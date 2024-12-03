94d7e4462253fbb030cf01ae0b6722f549848edb
utkarshdagoat <utkarsh382004@gmail.com>
2024-12-03 21:01:02 +0530

    clean up

--- lib/src/image/transformations/utils.rs
+++
@@ -1,9 +0,0 @@
-#[macro_export]
-macro_rules! normalize {
-    ($r: expr) => {
-        ($r as f32) / (255f32)
-    };
-    ($r: expr, $factor:  expr) => {
-        ($r as f32) / ($factor as f32)
-    };
-}
